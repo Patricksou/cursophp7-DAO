25760b397927dfb567cd6eb608444828d86a0d1b
Patrick Souza <tricksouza82@gmail.com>
2022-07-04 23:33:51 -0400

    Métodos de lista

--- index.php
+++ index.php
@@ -2,9 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
-$root->loadbyId(3);
+//Carrega somente um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de usuários
+//$lista =  Usuario::getList();
+//echo json_encode($lista);
 
+//Carrega uma lista de usuários pelo login
+//$search = Usuario::search("us");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "54321");
+
+echo $usuario;
 ?>
